846eb345b9dbcecbf1c46c78c60df442a5e74e8c
Oliver Salvaterra <Oliver.Salvaterra@GMR.local>
2020-10-02 14:32:12 -0700

    Add project files.

---
+++ HeapsonHeaps/Heap.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeapsonHeaps
+{
+    class Heap
+    {
+        public List<int> list = new List<int>();
+        public Heap() { }
+
+        public int Count => list.Count;
+
+        public void insert(int num)
+        {
+            list.Add(num);
+
+            up(list.Count - 1);
+        }
+
+        public void up(int i)
+        {
+            if(i <= 0)
+            {
+                return;
+            }
+
+            if(list[i] < list[(i - 1)/2])
+            {
+                int temp = list[(i - 1)/2];
+                list[(i - 1) / 2] = list[i];
+                list[i] = temp;
+                i = (i-1)/2;
+            }
+            else
+            {
+                return; 
+            }
+
+            up(i);
+        }
+
+        public int pop()
+        {
+            int temp = list[0];
+            list[0] = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            down(0);
+
+            return temp;
+        }
+
+        public void down(int i)
+        {
+            if(i*2 + 1 > list.Count-1)
+            {
+                return;
+            }
+
+            if (list[i] > list[i * 2 + 1])
+            {
+                int temp = list[i];
+                list[i] = list[i * 2 + 1];
+                list[i * 2 + 1] = temp;
+                i = i * 2 + 1;
+            }
+            else if (i * 2 + 2 < list.Count && list[i] > list[i * 2 + 2])
+            {
+                int temp = list[i];
+                list[i] = list[i * 2 + 2];
+                list[i * 2 + 2] = temp;
+                i = i * 2 + 2;
+            }
+            else
+            {
+                return;
+            }
+
+            down(i);
+        }
+    }
+}
---
+++ HeapsonHeaps/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HeapsonHeaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heap h = new Heap();
+
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                h.insert(rnd.Next(1, 20));
+            }
+
+            while (h.Count > 0)
+            {
+                Console.WriteLine(h.pop());
+            }
+        }
+    }
+}
